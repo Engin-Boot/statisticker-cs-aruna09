1970161ea2bc158ec91574345b4aed23db23d768
Maurya <Aruna.Maurya@philips.com>
2020-09-10 16:32:52 +0530

    Added relevant functions

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
+        public Stats setStats;
         public Stats CalculateStatistics(List<float> numbers) {
             //Implement statistics here
+            if(numbers.Count() == 0)
+            {
+                setStats.max = float.NaN;
+                setStats.max = float.NaN;
+                setStats.max = float.NaN;
+                return setStats;
+            }
+            else
+            {
+                ReturnAverage(numbers);
+                ReturnMax(numbers);
+                ReturnMin(numbers);
+                return setStats;
+            }
+        }
+        private void ReturnAverage(List<float> numbers)
+        {
+            float computedAverage = numbers.Sum() / numbers.Count();
+            setStats.average = computedAverage;
+        }
+        private void ReturnMax(List<float> numbers)
+        {
+            float max = numbers.Max();
+            setStats.max = max;
+        }
+        private void ReturnMin(List<float> numbers)
+        {
+            float min = numbers.Min();
+            setStats.min = min;
         }
     }
 }
---
+++ Statistics/Stats.cs
@@ -0,0 +1,9 @@
+﻿namespace Statistics
+{
+    public class Stats
+    {
+        public float average { get; set; }
+        public float max { get; set; }
+        public float min { get; set; }
+    }
+}
